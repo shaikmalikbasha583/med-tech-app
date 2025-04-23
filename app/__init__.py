189d9b50e381f300d048106c027c55338c7ec8c5
shaikmalikbasha583 <shaikmalikbasha583@gmail.com>
2025-04-23 10:34:53 +0530

    app name updated

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import logging
 from flask import Flask, render_template
 
 from app.data.branches import data
+from app.utils import constants as c
 from config import Config
 
 
@@ -13,7 +14,7 @@ def create_app():
     @app.route("/")
     def index():
         logging.info("Returning Home Page")
-        return render_template("index.html", data=data)
+        return render_template("index.html", data=data, c=c)
 
     # db.init_app(app)
     # login_manager.init_app(app)
---
+++ app/utils/constants.py
@@ -0,0 +1,1 @@
+APP_NAME = "MedTech App"
